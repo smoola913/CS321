6c4217a29f2f10f06ef38bfd3054ceaf4d7eab93
smoola913 <smoola@gmu.edu>
2023-10-20 15:31:05 -0400

    templates + tests made for creating and sending reports

--- src/UnitTesting.java
+++ src/UnitTesting.java
@@ -6,25 +6,43 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class UnitTesting {
+
+    public Workflow workflow;
     /*
      * Workflow object tests
      */
 
     @Test
-    public void ReviewCreatedTest() {
+    public void ReportCreatedTest() {
+        //create report
+        DivorceReport report = new DivorceReport();
 
+        assertTrue(checkReport(report));
+        //assert whether review was created
     }
 
     @Test
     public void WorkflowCreatedTest(){
+        //create workflow
 
+        //assert whether workflow was created
     }
+
     /*
      * Business object tests
      */
 
     @Test
-    public void ReviewReceivedTest(){
+    public void ReportReceivedTest(){
+        //create review
+        DivorceReport report = new DivorceReport();
+        Boolean validated = checkReport(report);
+
+        //send review
+        nextStep(validated);
+
+        //assert whether review was received
+        assertEquals(report, getDivorceReport(workflow));
 
     }
 }
