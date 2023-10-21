c7389fc75f35cdfe11ac76a52bb478bd095f71f5
Dominias <50306373+Dominias@users.noreply.github.com>
2023-10-20 20:07:13 -0400

    Workflow Test
    I made three tests for the workflow object that check to see if it is always created and returned correctly.

--- src/UnitTesting.java
+++ src/UnitTesting.java
@@ -2,53 +2,123 @@ package src;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
 public class UnitTesting {
 
-    /*
-     * Workflow object tests
-     */
+    // Workflow
 
+    // Tests that the workflow is created from DataEntry's createWorkflowItem
+    // method.
     @Test
-    public void ReportCreatedTest() {
-        //create report
-        DivorceReport report = new DivorceReport();
+    public void WorkflowCreatedTest() {
+        DataEntry dataEntry = new DataEntry();
 
-        assertTrue(checkReport(report));
-        //assert whether review was created
+        DivorceReport report = new DivorceReport("User", "Spouse", "Status");
+
+        Workflow workflow = dataEntry.createWorkflowItem(report);
+
+        assertNotNull(workflow);
+    }
+
+    // Tests that the workflow is returned from Review
+    @Test
+    public void WorkflowReviewReturnedTest() {
+        Workflow workflow = new Workflow();
+
+        DivorceReport report = new DivorceReport("User", "Spouse", "Status");
+
+        Review review = new Review(report, true, workflow);
+
+        review.nextStep(review.getValidated());
+
+        assertEquals("Approval", workflow.getStep());
+    }
+
+    // Tests that the workflow is returned from Approval if not validated
+    @Test
+    public void WorkflowApprovalReturnedTest() {
+        Workflow workflow = new Workflow();
+
+        DivorceReport report = new DivorceReport("User", "Spouse", "Status");
+
+        Approval approval = new Approval(report, false, workflow);
+
+        approval.nextStep(approval.getValidated());
+
+        assertEquals("Review", workflow.getStep);
+
     }
 
     @Test
-    public void WorkflowCreatedTest(){
-        //create workflow
+    public void WorkflowTest() {
+        DivorceReport report = new DivorceReport("Bob", "Linda", "Status");
 
-        //assert whether workflow was created
+        Workflow workflow = new Workflow(report, "Review");
+
+        assertEquals(report, workflow.getReport());
     }
 
-    /*
-     * Business object tests
-     */
+    // DivorceReport
+    @Test
+    public void DivorceReportTest() {
+
+        DivorceReport report = new DivorceReport("Mary", "John", "Status");
+
+        assertEquals("Mary", report.getUserInformation());
+        assertEquals("John", report.getSpouseInformation());
+        assertEquals("Status", report.getMarriageStatus());
+    }
 
     @Test
-    public void ReportReceivedTest(){
-        //create review
-        DivorceReport report = new DivorceReport();
-        Boolean validated = checkReport(report);
+    public void DivorceReportTest_1() {
 
-        //send review
-        nextStep(validated);
+        DivorceReport report = new DivorceReport("Jeff", "Hellen", "Status");
 
-        //assert whether review was received
-        assertEquals(report, getDivorceReport(workflow));
-
+        assertEquals("Jeff", report.getUserInformation());
+        assertEquals("Hellen", report.getSpouseInformation());
+        assertEquals("Status", report.getMarriageStatus());
     }
 
-    public static void main(String[] args){
-        Workflow workflow;
-        ReportCreatedTest();
-        WorkflowCreatedTest();
-        ReportReceivedTest();
+    @Test
+    public void DivorceReportTest_2() {
+
+        DivorceReport report = new DivorceReport("", "", "");
+
+        assertEquals("", report.getUserInformation());
+        assertEquals("", report.getSpouseInformation());
+        assertEquals("", report.getMarriageStatus());
+    }
+
+    // Review
+    @Test
+    public void ReviewTest() {
+
+        Review review = new Review("Bob", true, 1539845);
+
+        assertEquals("Bob", review.getName());
+        assertEquals(1539845, review.getID());
+        assertTrue(review.getMaritalStatus());
+    }
+
+    // DataEntry
+    @Test
+    public void DataEntryTest1() {
+        DataEntry dataEntry = new DataEntry("Bob", "Lisa", "Marry");
+
+        assertEquals("Bob", dataEntry.getUserInformation());
+        assertEquals("Lisa", dataEntry.getSpouseInformation());
+        assertEquals("Marry", dataEntry.getAction());
+    }
+
+    @Test
+    public void DataEntryTest2() {
+        DataEntry dataEntry = new DataEntry("Bob", "Lisa", "Divorce");
+
+        assertEquals("Bob", dataEntry.getUserInformation());
+        assertEquals("Lisa", dataEntry.getSpouseInformation());
+        assertEquals("Divorce", dataEntry.getAction());
     }
 }
