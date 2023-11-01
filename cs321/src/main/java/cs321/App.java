ab1501b65db31aa33a87556c38d954d42914bf69
Nathaniel Young <nyoung27@gmu.edu>
2023-10-31 23:55:23 -0400

    Testing

--- cs321/src/main/java/cs321/App.java
+++ cs321/src/main/java/cs321/App.java
@@ -1,38 +1,82 @@
 package cs321;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.application.Application; 
+import static javafx.application.Application.launch; 
+import javafx.geometry.Insets; 
+import javafx.geometry.Pos; 
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
-public class App extends Application {
-
-    private static Scene scene;
-
-    @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.control.PasswordField; 
+import javafx.scene.layout.GridPane; 
+import javafx.scene.text.Text; 
+import javafx.scene.control.TextField; 
+import javafx.stage.Stage;  
+         
+public class LoginPage extends Application { 
+   @Override 
+   public void start(Stage stage) {      
+      //creating label email 
+      Text text1 = new Text("Email");       
+      
+      //creating label password 
+      Text text2 = new Text("Password"); 
+       
+      //Creating Text Filed for email        
+      TextField textField1 = new TextField();       
+      
+      //Creating Text Filed for password        
+      PasswordField textField2 = new PasswordField();  
+       
+      //Creating Buttons 
+      Button button1 = new Button("Submit"); 
+      Button button2 = new Button("Clear");  
+      
+      //Creating a Grid Pane 
+      GridPane gridPane = new GridPane();    
+      
+      //Setting size for the pane 
+      gridPane.setMinSize(400, 200); 
+      
+      //Setting the padding  
+      gridPane.setPadding(new Insets(10, 10, 10, 10)); 
+      
+      //Setting the vertical and horizontal gaps between the columns 
+      gridPane.setVgap(5); 
+      gridPane.setHgap(5);       
+      
+      //Setting the Grid alignment 
+      gridPane.setAlignment(Pos.CENTER); 
+       
+      //Arranging all the nodes in the grid 
+      gridPane.add(text1, 0, 0); 
+      gridPane.add(textField1, 1, 0); 
+      gridPane.add(text2, 0, 1);       
+      gridPane.add(textField2, 1, 1); 
+      gridPane.add(button1, 0, 2); 
+      gridPane.add(button2, 1, 2); 
+       
+      //Styling nodes  
+      button1.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+      button2.setStyle("-fx-background-color: darkslateblue; -fx-text-fill: white;"); 
+       
+      text1.setStyle("-fx-font: normal bold 20px 'serif' "); 
+      text2.setStyle("-fx-font: normal bold 20px 'serif' ");  
+      gridPane.setStyle("-fx-background-color: BEIGE;"); 
+       
+      //Creating a scene object 
+      Scene scene = new Scene(gridPane); 
+       
+      //Setting title to the Stage 
+      stage.setTitle("CSS Example"); 
+         
+      //Adding scene to the stage 
+      stage.setScene(scene);
+      
+      //Displaying the contents of the stage 
+      stage.show(); 
+   }      
+   public static void main(String args[]){ 
+      launch(args); 
+   } 
 }
--- cs321/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cs321 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cs321 to javafx.fxml;
-    exports cs321;
-}
