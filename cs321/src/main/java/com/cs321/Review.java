e4bf69b55d14fc89f281c50ca4a74cc25086003c
Dominias <50306373+Dominias@users.noreply.github.com>
2023-11-03 05:10:59 -0400

    Review JavaFX

--- cs321/src/main/java/com/cs321/Review.java
+++ cs321/src/main/java/com/cs321/Review.java
@@ -3,13 +3,12 @@ package com.cs321;
 public class Review {
     private DivorceReport report;
     private Boolean validated;
-
     private Workflow workflow;
 
-    public Review(DivorceReport report, Boolean validated, Workflow workflow) {
-        this.report = report;
+    public Review(Boolean validated, Workflow workflow) {
         this.validated = validated;
         this.workflow = workflow;
+        this.report = workflow.getReport();
     }
 
     public DivorceReport getReport() {
@@ -37,7 +36,7 @@ public class Review {
     }
 
     public void showReviewScreen() {
-
+        // Not needed for Sprint 1.
     }
 
     public void nextStep(Boolean validated) {
--- cs321/src/main/java/com/cs321/ReviewController.java
+++ cs321/src/main/java/com/cs321/ReviewController.java
@@ -1,12 +1,52 @@
 package com.cs321;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class ReviewController {
+    private DivorceReport report;
+    private Workflow workflow;
+    private Review review;
 
     @FXML
-    private void Test() throws IOException {
+    private TextField user;
+    @FXML
+    private TextField spouse;
+    @FXML
+    private TextField marriage;
 
+    public ReviewController() {
+        report = new DivorceReport("John", "Jane", "Married");
+        workflow = new Workflow(report, "Review");
+
+        review = new Review(false, workflow);
+    }
+
+    @FXML
+    private void view(ActionEvent event) throws IOException {
+        user.setText(review.getReport().getUserInformation());
+        spouse.setText(review.getReport().getSpouseInformation());
+        marriage.setText(review.getReport().getMarriageStatus());
+    }
+
+    @FXML
+    private void edit(ActionEvent event) throws IOException {
+        if (user.getText() != "")
+            review.getReport().setUserInformation(user.getText());
+        if (spouse.getText() != "")
+            review.getReport().setSpouseInformation(spouse.getText());
+        if (marriage.getText() != "")
+            review.getReport().setMarriageStatus(marriage.getText());
+
+        System.out.println(report);
+    }
+
+    @FXML
+    private void finish() {
+        System.out.println(report);
+        System.exit(0);
     }
 }
