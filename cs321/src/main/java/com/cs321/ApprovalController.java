64eb5f85bb559a83aa94b4d4a16c79171c80104c
smoola913 <smoola@gmu.edu>
2023-11-16 05:31:32 -0500

    approval screen now functions with workflow interacts with other screens optimized for mac

--- cs321/src/main/java/com/cs321/ApprovalController.java
+++ cs321/src/main/java/com/cs321/ApprovalController.java
@@ -8,21 +8,38 @@ import javafx.scene.control.TextField;
 
 public class ApprovalController {
 
+    private Approval approval;
+
     @FXML
     private TextField client_name;
     @FXML
-    private TextField current_status;
+    private TextField spouse_name;
     @FXML
     private TextField desired_status;
 
+    public ApprovalController() {
+        this.approval = new Approval();
+    }
+
     @FXML
     private void sendApproval(ActionEvent event) throws IOException {
-        System.out.println("APPROVED: " + client_name.getText() + " would like to change their marriage status from " + current_status.getText() + " to " + desired_status.getText() + ".");
+        System.out.println("APPROVED: " + client_name.getText() + " would like to change their marriage status with " + spouse_name.getText() + " to " + desired_status.getText() + ".");
     }
 
     @FXML
     private void denyApproval(ActionEvent event) throws IOException {
-        System.out.println("DENIED: " + client_name.getText() + " would like to change their marriage status from " + current_status.getText() + " to " + desired_status.getText() + ".");
+        System.out.println("DENIED: " + client_name.getText() + " would like to change their marriage status with " + spouse_name.getText() + " to " + desired_status.getText() + ".");
         System.out.println("Form has been sent back for review.");
+    }
+
+    @FXML
+    private void load(){
+        approval.setReport(Workflow.getApprovalItem());
+    }
+
+    @FXML void fill(){
+        client_name.setText(approval.getReport().getUserInformation());
+        spouse_name.setText(approval.getReport().getSpouseInformation());
+        desired_status.setText(approval.getReport().getMarriageStatus());
     }
 }
